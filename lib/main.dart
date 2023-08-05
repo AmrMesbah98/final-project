6e8d7531a0253196416833d4bfb3c1a0cf7143af
Amr Mesbah <amrmesbah33@gmail.com>
2023-08-05 16:16:32 +0300

    master

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:finalproject/presntation/view/splash/splash.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Splash(),
+    );
+  }
+}
